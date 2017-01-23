c4da6597babd7c75ca4783a9a5640a2c5ce44675
SHERRY <swasieko@gmail.com>
2017-01-23 23:54:23 +0000

    Implemented ability to upload csb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  resources :companies do
+    collection { post :import }
+  end
+    
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
-
   get 'welcome/features'
 
   resources :invoices
